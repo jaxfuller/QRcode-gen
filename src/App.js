26cc645c51f9658153a980c2301d680fa0114b54
jaxfuller <jaxful@gmail.com>
2023-04-02 18:13:16 -0600

    code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
-
+  
 function App() {
+  const [temp, setTemp] = useState("");
+  const [word, setWord] = useState("");
+  const [size, setSize] = useState(200);
+  const [color, setColor] = useState ("#8b0000");
+  const [bgColor, setBgColor] = useState("ffb6c1");
+  const [qrCode, setQrCode] = useState("");
+
+  //changes link to match inputs
+  useEffect(() => {
+    setQrCode
+ (`http://api.qrserver.com/v1/create-qr-code/?data=${word}!&size=${size}x${size}&bgcolor=${bgColor}&color=${color}`);
+  }, [word, size, bgColor, color]);
+  
+//updates input when user clicks
+  function handleClick() {
+    setWord(temp);
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <h1>QR Code Generator</h1>
+      <h3> This is completed using the free, open-source API "create-qr-code"</h3>
+      <div className="input-box">
+        <div className="gen">
+          <input type="text" onChange={
+            (e) => {setTemp(e.target.value)}}
+            placeholder="Enter Linked to Convert" />
+          <button className="button" 
+            onClick={handleClick}>
+            Generate
+          </button>
+        </div>
+        <div className="extra">
+          <h5>Background Color:</h5>
+          <input type="color" onChange={(e) => 
+          { setBgColor(e.target.value.substring(1)) }} />
+          <h5>Color:</h5>
+          <input type="color" onChange = {(e) =>
+          { setColor(e.target.value.substring(1)) }} />
+          <h5>Dimension:</h5>
+          <input type="range" min="100" max="400"
+           value={size} onChange={(e) => 
+           {setSize(e.target.value)}} />
+        </div>
+      </div>
+      <div className="output-box">
+        <img src={qrCode} alt="" />
+        <a href={qrCode} download="QRCode">
+          <button type="button">Download</button>
         </a>
-      </header>
+      </div>
     </div>
   );
 }
-
+  
 export default App;
